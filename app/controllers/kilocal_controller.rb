1f47f6cc3e3166d5c487043676479ed6f2b56311
Publicover <jameslpublicover@gmail.com>
2016-03-04 13:26:14 -0500

    fill out kilocal controller

--- app/controllers/kilocal_controller.rb
+++ app/controllers/kilocal_controller.rb
@@ -1,11 +1,10 @@
 class KilocalController < ApplicationController
   def new
-  end
-
-  def create
+    @kilocal = Kilocal.new
   end
 
   def index
+    @kilocals = Kilocal.all
   end
 
   def show
@@ -13,4 +12,39 @@ class KilocalController < ApplicationController
 
   def edit
   end
+
+  def create
+    @kilocal = Kilocal.new(kilocal_params)
+
+    respond_to do |format|
+      if @kilocal.save
+        format.html { redirect_to @kilocal, notice: 'Kilocal was successfully created.' }
+        format.json { render :show, status: :created, location: @kilocal }
+      else
+        format.html { render :new }
+        format.json { render json: @kilocal.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  def update
+    respond_to do |format|
+      if @kilocal.update(kilocal_params)
+        format.html { redirect_to @kilocal, notice: 'Kilocal was successfully updated.' }
+        format.json { render :show, status: :ok, location: @kilocal }
+      else
+        format.html { render :edit }
+        format.json { render json: @kilocal.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  def destroy
+    @kilocal.destroy
+    respond_to do |format|
+      format.html { redirect_to kilocals_url, notice: 'Kilocal was successfully destroyed.' }
+      format.json { head :no_content }
+    end
+  end
+
 end
