5ffb7ade312f45ce9c04976ebca5ba91c5373c9a
Geoffrey Arnold <geoffrey.s.arnold@gmail.com>
2016-03-05 20:03:54 -0500

    Add methods to variables in dashboard controller.

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -1,7 +1,7 @@
 class DashboardController < ApplicationController
   def index
-    @net_daily_calories = Kilacal.
-    @daily_steps_weekly_comparison = DailyStep.
-    @weight_reduction_left = Weight.
+    @net_daily_calories = Kilacal.net_daily_calories
+    @daily_steps_weekly_comparison = DailyStep.steps_comparison
+    @weight_reduction_left = Weight.weight_left_until_goal
   end
 end
